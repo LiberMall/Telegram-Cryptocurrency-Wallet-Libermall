28c546562b650efc2bb698b52b74ff7573ce5829
Billionaire DendiBoss <3163941+DeFiTON@users.noreply.github.com>
2025-08-23 20:32:33 +0500

    refactor: load config from environment

--- botdata.php
+++ botdata.php
@@ -1,22 +1,30 @@
 <?php
-$xPayMerchantId = '5a6200fe-0798-4b4a-b1c3-2779d42af64a';
-$xPayPrivateKey = '71ba018a2bb3ffb52fcceb9aa3b574bc';
-$tonapikey = 'e52c8854142eba7ba3c015c1b0b046d31e0333a6d25fce4e7872cec7cd9c39e4';
-$mainWallet = 'EQADeT9xqZ4wAam-bCXbTjNhUTN03K33EYQmnL5Q8udk5E2T';
-$genseed = 'tide viable airport master inch weather citizen hood slot wire fatal toy below bring same vocal amused tourist opera seed burger october collect master';
-$tgrbep20fee = 2; // USD
-$tgrtonfee = 0.1; // TON
-$tonfee = 0.1; // TON
-$tegromoney_shopid = "58897C1D797CA1F09A12269C8E40BA69";
-$tegromoney_secretkey = "p3fUjzEM";
-$chequefee = 0.02;
-$stakingfee[0][0] = 15;
-$stakingfee[0][1] = 18;
-$stakingfee[0][2] = 21;
-$stakingfee[0][3] = 24;
-$stakingfee[1][0] = 9;
-$stakingfee[1][1] = 10;
-$stakingfee[1][2] = 11;
-$stakingfee[1][3] = 12;
-$exchangefee = 0.005;
- ?>
+require_once __DIR__ . '/env.php';
+
+$xPayMerchantId = getenv('XPAY_MERCHANT_ID');
+$xPayPrivateKey = getenv('XPAY_PRIVATE_KEY');
+$tonapikey = getenv('TON_API_KEY');
+$mainWallet = getenv('MAIN_WALLET');
+$genseed = getenv('GEN_SEED');
+
+$tgrbep20fee = getenv('TGR_BEP20_FEE');
+$tgrtonfee = getenv('TGR_TON_FEE');
+$tonfee = getenv('TON_FEE');
+
+$tegromoney_shopid = getenv('TEGROMONEY_SHOPID');
+$tegromoney_secretkey = getenv('TEGROMONEY_SECRETKEY');
+
+$chequefee = getenv('CHEQUE_FEE');
+
+$stakingfee[0][0] = getenv('STAKING_FEE_0_0');
+$stakingfee[0][1] = getenv('STAKING_FEE_0_1');
+$stakingfee[0][2] = getenv('STAKING_FEE_0_2');
+$stakingfee[0][3] = getenv('STAKING_FEE_0_3');
+$stakingfee[1][0] = getenv('STAKING_FEE_1_0');
+$stakingfee[1][1] = getenv('STAKING_FEE_1_1');
+$stakingfee[1][2] = getenv('STAKING_FEE_1_2');
+$stakingfee[1][3] = getenv('STAKING_FEE_1_3');
+
+$exchangefee = getenv('EXCHANGE_FEE');
+?>
+
--- global.php
+++ global.php
@@ -1,6 +1,8 @@
-<?
-$hostName = 'localhost';
-$userName = 'tegromoneybot_bot';
-$password = 'TV0Up5ARw036c';
-$databaseName = 'tegromoneybot_bot';
-?>
+<?php
+require_once __DIR__ . '/env.php';
+
+$hostName   = getenv('DB_HOST');
+$userName   = getenv('DB_USER');
+$password   = getenv('DB_PASS');
+$databaseName = getenv('DB_NAME');
+
