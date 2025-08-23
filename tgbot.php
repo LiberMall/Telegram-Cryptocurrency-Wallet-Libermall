09ca7318edad921b00fa25f61c8d797a32c297ae
Billionaire DendiBoss <3163941+DeFiTON@users.noreply.github.com>
2025-08-23 21:48:53 +0500

    refactor: manage subscribers list in json

--- tgbot.php
+++ tgbot.php
@@ -23,6 +23,7 @@ include 'func_cheque.php';
 include 'func_staking.php';
 include 'func_exchange.php';
 include 'func_exchange2.php';
+include 'func_subscribers.php';
 
 #################################
 
@@ -69,35 +70,7 @@ checkInlineQuery();
 ############### START ###############
 if( preg_match("/\/start/i", $data['message']['text'] )){
 
-//register subscriber
-$subscribersFile = 'subscribers.json';
-$first = isset($data['message']['from']['first_name']) ? $data['message']['from']['first_name'] : '';
-$last = isset($data['message']['from']['last_name']) ? $data['message']['from']['last_name'] : '';
-$usern = isset($data['message']['from']['username']) ? $data['message']['from']['username'] : '';
-$newrecord = [
-        'chat_id' => (int)$chat_id,
-        'name' => trim(filter_var($first." ".$last, FILTER_SANITIZE_FULL_SPECIAL_CHARS)),
-        'username' => trim(filter_var($usern, FILTER_SANITIZE_FULL_SPECIAL_CHARS))
-];
-$subscribers = [];
-if(file_exists($subscribersFile)){
-        $fileData = file_get_contents($subscribersFile);
-        $subscribers = json_decode($fileData, true);
-        if(!is_array($subscribers)) $subscribers = [];
-}
-$exists = false;
-foreach($subscribers as $record){
-        if($record['chat_id'] === $newrecord['chat_id']){
-                $exists = true;
-                break;
-        }
-}
-if(!$exists){
-        $subscribers[] = $newrecord;
-        file_put_contents($subscribersFile, json_encode($subscribers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE), LOCK_EX);
-        @chmod($subscribersFile, 0600);
-}
-//register subscriber
+registerSubscriber($chat_id, $data);
 
 $r = saveReferral($data);
 
